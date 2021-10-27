c12e40a6ed47d09fad964c57cccd473a149046eb
Halucyjan <jakubgrzyb1999@gmail.com>
2021-10-28 00:18:56 +0200

    start with controler

--- Diffusion2D/Controler.cpp
+++ Diffusion2D/Controler.cpp
@@ -1,1 +1,37 @@
 #include "Controler.h"
+
+Controler::Controler()
+	:width(0), height(0), numberOfParticle(0)
+{
+
+}
+
+Controler::Controler(int w, int h, int n)
+	: width(w), height(h), numberOfParticle(n)
+{
+	if (this->checkBoardAndParticle())
+	{
+		this->setBoard();
+		this->setParticle();
+	}
+	else
+		printf("ERROR");
+}
+
+void Controler::setParticle()
+{
+
+}
+
+void Controler::setBoard()
+{
+
+}
+
+bool Controler::checkBoardAndParticle()
+{
+	if ((this->height * this->width) > this->numberOfParticle)
+		return true;
+	else
+		return false;
+}
--- Diffusion2D/Controler.h
+++ Diffusion2D/Controler.h
@@ -1,5 +1,17 @@
 #pragma once
+
+#include <iostream>
+
 class Controler
 {
+public:
+	Controler();
+	Controler(int, int, int);
+	void setParticle();
+	void setBoard();
+	bool checkBoardAndParticle();
+private:
+	int width, height, numberOfParticle;
 };
 
+	
